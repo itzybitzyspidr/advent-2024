f20514b77a971594f640d3db79c341627739eb3f
Tyler Webb <tyler.webb@fusion5.com.au>
2024-11-24 16:10:41 +1100

    feat: :sparkles: overcooked day7

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { program } from "commander";
-import { d6p1 } from "./previous-years/2015/solutions/day6/solution";
+import { d7p1 } from "./previous-years/2015/solutions/day7/solution";
 
 program.action( async () => {
-  console.log(d6p1());
+  console.log(d7p1());
 });
 
 program.parse();
---
+++ src/previous-years/2015/solutions/day7/solution.ts
@@ -0,0 +1,119 @@
+import { getInputAsLines } from "../../../../helpers/read-inputs";
+
+enum Operator {
+  AND = 'AND',
+  OR = 'OR',
+  LSHIFT = 'LSHIFT',
+  RSHIFT = 'RSHIFT',
+  PASS = 'PASS',
+  NOT = 'NOT',
+};
+
+interface IGate {
+  id: string;
+  operator: Operator;
+  inputGateIds: string[];
+  rawInputs: number[];
+  calculatedValue?: number;
+};
+
+export function d7p1(): number {
+  const lines = getInputAsLines(7).body;
+  const gateMap = createGateMap(lines);
+  
+  return evalutateGate('a', gateMap);
+}
+
+function createGateMap(lines: string[]): Map<string, IGate> {
+  const gateMap = new Map<string, IGate>();
+
+  for (const line of lines) {
+    const [source, destination] = line.split(' -> ');
+    
+    const instructionMatch = source.match(/AND|OR|LSHIFT|RSHIFT|NOT/);
+    let operator: Operator;
+    if (instructionMatch) {
+      operator = Operator[instructionMatch.toString() as Operator];
+    } else {
+      operator = Operator.PASS;
+    }
+
+    const sendingGateIds = source.match(/[a-z]+/g);
+    const inputValues = source.match(/[0-9]+/);
+
+    const destinationGate: IGate = {
+      id: destination,
+      operator,
+      rawInputs: [],
+      inputGateIds: [],
+    };
+
+    if (inputValues) {
+      for (const input of inputValues) {
+        destinationGate.rawInputs.push(+input);
+      }
+    }
+
+    if (sendingGateIds) {
+      destinationGate.inputGateIds = sendingGateIds.map((id) => id);
+    }
+    gateMap.set(destination, destinationGate);
+  }
+  return gateMap;
+}
+
+// Recursively evalute all input gates.
+function evalutateGate(id: string, gateMap: Map<string, IGate>): number {
+  const gate = gateMap.get(id);
+  if (!gate) {
+    throw new Error(`Gate ${id} was not found.`);
+  }
+
+  if (gate.calculatedValue) {
+    return gate.calculatedValue;
+  }
+
+  const incomingValues = gate.inputGateIds.map((inputId) => evalutateGate(inputId, gateMap));
+  const rawInputs = gate.rawInputs;
+
+  let v = 0;
+  switch (gate.operator) {
+    case Operator.AND:
+      v = incomingValues[0];
+      for (const value of [...incomingValues.slice(1), ...rawInputs]) {
+        v = v & value;
+      }
+      break;
+    case Operator.OR:
+      for (const value of [...incomingValues, ...rawInputs]) {
+        v = v | value;
+      }
+      break;
+    case Operator.LSHIFT:
+      v = incomingValues[0] << gate.rawInputs[0];
+      break;
+    case Operator.RSHIFT:
+      v = incomingValues[0] >> gate.rawInputs[0];
+      break;
+    case Operator.NOT:
+      if (incomingValues.length > 0) {
+        v = ~incomingValues[0];
+      } else {
+        v = ~rawInputs[0];
+      }
+      break;
+    case Operator.PASS:
+      if (incomingValues.length > 0) {
+        v = incomingValues[0];
+      } else {
+        v = rawInputs[0];
+      }
+
+      break;
+    default:
+      throw new Error(`Uncaught operator: ${gate.operator}`);
+  }
+
+  gate.calculatedValue = v;
+  return v;
+}
