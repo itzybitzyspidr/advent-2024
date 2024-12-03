8f64509e7650f152c41f01279d877f7f4148c13b
Tyler Webb <tyler.webb@fusion5.com.au>
2024-12-03 21:18:07 +1100

    feat: :sparkles: 🤢🤢🤢

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { program } from "commander";
 import { y2024d2p1, y2024d2p2 } from "./solutions/day2/solution";
+import { y2024d3p1, y2024d3p2 } from "./solutions/day3/solution";
 
 program.action(() => {
-  console.log(y2024d2p2());
+  console.log(y2024d3p2());
 });
 
 program.parse();
---
+++ src/solutions/day3/solution.ts
@@ -0,0 +1,50 @@
+import { getInputAsString } from "../../helpers/read-inputs";
+import { getMatches } from "../../helpers/search";
+
+export function y2024d3p1(): number {
+  const input = getInputAsString(3);
+  const muls = getMatches(input, /mul\([\d]{1,3},[\d]{1,3}\)/g);
+  let tot = 0;
+  for (const mul of muls) {
+    const [l, r] = getMatches(mul, /[\d]+/g);
+    tot += +l * +r;
+  }
+  return tot;
+}
+
+export function y2024d3p2(): number {
+  const input = getInputAsString(3);
+  const doRegex = /do\(\)/;
+  const mulOrDontRegex = /don\'t\(\)|mul\([\d]{1,3},[\d]{1,3}\)/;
+
+  let idx = 0;
+  let enabled = true;
+  let tot = 0;
+  while (idx < input.length) {
+    console.log(idx)
+    if (enabled) {
+      const nextInstruction = mulOrDontRegex.exec(input.slice(idx));
+
+      if (!nextInstruction) {
+        return tot;
+      }
+
+      if (nextInstruction[0].includes('mul')) {
+        const [l, r] = getMatches(nextInstruction[0], /[\d]+/g);
+        tot += +l * +r;
+      } else {
+        enabled = !enabled;
+      }
+      idx += nextInstruction.index + 1;
+    } else  {
+      const nextInstruction = doRegex.exec(input.slice(idx));
+      if (!nextInstruction) {
+        return tot;
+      }
+
+      enabled = !enabled;
+      idx += nextInstruction.index + 1;
+    }
+  }
+  return tot;
+}
