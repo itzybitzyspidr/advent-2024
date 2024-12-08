aae674a899adcfa5c852fcce27faae4c6ff65f37
Tyler Webb <tyler.webb@fusion5.com.au>
2024-12-08 23:29:48 +1100

    feat: :sparkles: added support for counting specific porperties of object

--- src/helpers/math.ts
+++ src/helpers/math.ts
@@ -1,8 +1,40 @@
+export interface IMathOptions {
+  compareProp?: string;
+}
+
 export function getPairDistance(number1: number, number2: number): number {
   return Math.abs(number1 - number2);
 }
 
-// Todo - Include support for objects and nested comparisons
-export function countSpecific<T>(input: T[], target: T): number {
-  return input.reduce((prev, cur) => prev += cur === target ? 1 : 0, 0);
+export function countSpecific<T>(input: T[], target: any, options?: IMathOptions): number {
+  if (typeof input[0] === typeof target) {
+    for (const v of input) {
+      if (typeof v !== typeof target) {
+        throw new Error(`Multiple different types detected in array: ${input}.`);
+      }
+    }
+    return input.reduce((prev, cur) => prev += cur === target ? 1 : 0, 0);
+  }
+
+  let values: any[] = [];
+  if (input[0] instanceof Object) {
+    if (!options || !options.compareProp) {
+      throw new Error('Property to sort by not provided for object.');
+    }
+
+    const props = Object.keys(input[0]);
+    if (!props.includes(options.compareProp)) {
+      throw new Error(`Property provided: '${options.compareProp}' was not found on object.`);
+    }
+
+    values = input.map((ob) => {
+      const v = ob[options.compareProp as keyof T];
+      if (typeof v === typeof target) {
+        return v;
+      }
+      throw new Error(`Type of ${ob[options.compareProp as keyof T]}: ${typeof options.compareProp} does not match type of ${target}: ${typeof target}.`);
+    });
+  }
+
+  return values.reduce((prev, cur) => prev += cur === target ? 1 : 0, 0);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { program } from "commander";
 import { y2024d8p2 } from "./solutions/day8/solution";
 import { getInputAsGrid } from "./helpers/read-inputs";
+import { countSpecific } from "./helpers/math";
 
 
 program.action(() => {
-  console.log(getInputAsGrid(8).body);
+  const arr = [
+    {
+      a: 'bean'
+    },
+    {
+      a: 'bean'
+    },
+    {
+      a: 'wolf'
+    }
+  ];
+  console.log(countSpecific(arr, 'bean', { compareProp: 'a' }))
 });
 
 program.parse();
